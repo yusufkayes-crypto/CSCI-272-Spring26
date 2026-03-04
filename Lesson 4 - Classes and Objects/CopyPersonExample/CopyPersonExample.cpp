86b612f7091818b69889fa37d8f950788e9798b1
yusufkayes-crypto <yusufkayes@gmail.com>
2026-03-03 22:55:45 -0500

    Create CopyPersonExample.cpp for class demonstration
    Added CopyPersonExample.cpp demonstrating class usage and member functions.

---
+++ Lesson 4 - Classes and Objects/CopyPersonExample/CopyPersonExample.cpp
@@ -0,0 +1,32 @@
+/*
+Shadman Yusuf Kayes
+Course: CSCI 272 (2)
+Lesson number: Lesson 4
+Code Topic: Copy PersonExample
+Date: 3/3/2026
+*/
+#include "Person.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    Person person1;
+    person1.introduce(); //calls the member function in class
+    
+    return 0;
+}
+/*
+Reflection:
+1. What I understood:
+ I understood how a simple class works using public data and member functions
+2. Difficulties:
+ In the beginning I forgot to add parenthesis when calling the member function in main()
+3. Research:
+ I searched online about member functions and how they differ from regular functions when called
+4. AI Usage:
+ Did not use AI for this section
+5. What I learned:
+ I learned the differences of member function and regular functions.
+
+
+*/
