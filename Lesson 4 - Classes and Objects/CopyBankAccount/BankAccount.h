5f550d962e479c2803c89c46701df9014015a2f6
yusufkayes-crypto <yusufkayes@gmail.com>
2026-03-03 22:22:06 -0500

    Add comments to BankAccount class members
    Added comments to clarify private member and getter method.

--- Lesson 4 - Classes and Objects/CopyBankAccount/BankAccount.h
+++ Lesson 4 - Classes and Objects/CopyBankAccount/BankAccount.h
@@ -2,14 +2,14 @@ using namespace std;
 
 class BankAccount{
     private:
-    double balance = 0.0;
+    double balance = 0.0; //not accesible outside of class
     
     public:
     void deposit(double amount){
         balance += amount;
     }
     
-    double getBalance() const {
+    double getBalance() const { //getter method
         return balance;
     }
     
